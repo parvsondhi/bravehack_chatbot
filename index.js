3db6c0b6e2648902e7da4382551e6ea44aafcf02
parvsondhi <parv@airbears2-10-142-160-185.airbears2.1918.berkeley.edu>
2016-11-18 22:44:00 -0800

    new

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ var changeClient = changeApi.createClient({
 changeClient.petitions.getByUrl(petitionUrl,
   function (err, res, body) {
 
-  	//console.log(body)
+  	console.log(body)
     //Process results here
   });
 
@@ -33,19 +33,19 @@ changeClient.petitions.getByUrl(petitionUrl,
 // 0f043943c7badca2efefe991ba9a68502df78abcf6572457d59179cc138c26a6
 
 
-changeClient.petitions.addSignature({
-	petition_id: 8930333,
-    api_secret: '69451c75c48efbfeda20747fc864da44998e28b436841985c44b3e4f94f6c054',
-    auth_key: '0f043943c7badca2efefe991ba9a68502df78abcf6572457d59179cc138c26a6',
-    source: 'https://hellohaven.herokuapp.com',
-    email: 'robin.oh@outlook.com',
-    first_name: 'Robin',
-    last_name: 'Oh',
-    city: 'Berkeley',
-    postal_code: 94703,
-    country_code: 'US' 
+// changeClient.petitions.addSignature({
+// 	petition_id: 8930333,
+//     api_secret: '69451c75c48efbfeda20747fc864da44998e28b436841985c44b3e4f94f6c054',
+//     auth_key: '0f043943c7badca2efefe991ba9a68502df78abcf6572457d59179cc138c26a6',
+//     source: 'https://hellohaven.herokuapp.com',
+//     email: 'robin.oh@outlook.com',
+//     first_name: 'Robin',
+//     last_name: 'Oh',
+//     city: 'Berkeley',
+//     postal_code: 94703,
+//     country_code: 'US' 
 
-	},
-  function (err, res, body) {
-    console.log(body);
-  });
+// 	},
+//   function (err, res, body) {
+//     console.log(body);
+//   });
